a277283df81e15cf7289ebf1b45e2b150d5b786a
So Youn Choi <manghatuhyo@gmail.com>
2023-12-02 14:48:34 +0100

    at least this one works, so its probably good to stop here

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ import cors from "cors";
 import crypto from 'crypto';
 import mongoose from 'mongoose';
 import bcrypt from 'bcrypt';
-
 const User = mongoose.model('User', {
   name: {
     type: String,
@@ -18,37 +17,38 @@ const User = mongoose.model('User', {
     default: () => crypto.randomBytes(128).toString('hex')
   }
 });
-
-// One-way encryption
-const user = new User({name: "Bob", password: bcrypt.hashSync("foobar")});
-user.save();
-
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// import avocadoSalesData from "./data/avocado-sales.json";
-// import booksData from "./data/books.json";
-// import goldenGlobesData from "./data/golden-globes.json";
-// import netflixData from "./data/netflix-titles.json";
-// import topMusicData from "./data/top-music.json";
-
-// Defines the port the app will run on. Defaults to 8080, but can be overridden
-// when starting the server. Example command to overwrite PORT env variable value:
-// PORT=9000 npm start
-const port = process.env.PORT || 8080;
-const app = express();
-
-// Add middlewares to enable cors and json body parsing
-app.use(cors());
-app.use(express.json());
-
-// Start defining your routes here
-app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
-});
-
-// Start the server
-app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
-});
-
-console.log(bcrypt.hashSync("foobar"));
+const saltRounds = 10;
+const password = "foobar";
+// Wrap the MongoDB-related operations in an asynchronous function
+async function initializeApp() {
+  try {
+    // Connect to MongoDB
+    await mongoose.connect('mongodb://localhost:27017/yourdatabase', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    // Hash the password
+    const salt = bcrypt.genSaltSync(saltRounds);
+    const hashedPassword = bcrypt.hashSync(password, salt);
+    // Log the hashed password to the console
+    console.log("Hashed Password:", hashedPassword);
+    // Create a new user with the hashed password
+    const user = new User({ name: "Bob", password: hashedPassword });
+    await user.save(); // Wait for the save operation to complete
+    // Set up Express app
+    const port = process.env.PORT || 8080;
+    const app = express();
+    app.use(cors());
+    app.use(express.json());
+    app.get("/", (req, res) => {
+      res.send("Hello Technigo!");
+    });
+    app.listen(port, () => {
+      console.log(`Server running on http://localhost:${port}`);
+    });
+  } catch (error) {
+    console.error("Error initializing the app:", error);
+  }
+}
+// Call the asynchronous function to initialize the app
+initializeApp();
