87f75e50d375f6f9cd0ad0f2a9b8395fa5d889f8
So Youn Choi <manghatuhyo@gmail.com>
2023-12-02 10:55:22 +0100

    go back to the original code from the video

--- server.js
+++ server.js
@@ -1,56 +1,54 @@
 import express from "express";
 import cors from "cors";
-import crypto from 'crypto';
-import mongoose from 'mongoose';
-import bcrypt from 'bcrypt';
+import crypto from 'crypto'
+import mongoose from 'mongoose'
+import bcrypt from 'bcrypt'
 
-// Connect to MongoDB
-mongoose.connect('mongodb://localhost:27017/your-database-name', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-
-// Define the User model
 const User = mongoose.model('User', {
-  name: {
+  name:{
     type: String,
-    unique: true,
+    unique: true
   },
-  password: {
+  password:{
     type: String,
-    required: true,
+    required: true
   },
-  accessToken: {
+  accessToken:{
     type: String,
-    default: () => crypto.randomBytes(128).toString('hex'),
-  },
-});
+    default: () => crypto.randomBytes(128).toString('hex')
+  }
+})
 
-// Create an Express app
+// One-way encryption
+const user = new User({name: "Bob", password: bcrypt.hashSync("foobar")})
+user.save()
+
+// If you're using one of our datasets, uncomment the appropriate import below
+// to get started!
+// import avocadoSalesData from "./data/avocado-sales.json";
+// import booksData from "./data/books.json";
+// import goldenGlobesData from "./data/golden-globes.json";
+// import netflixData from "./data/netflix-titles.json";
+// import topMusicData from "./data/top-music.json";
+
+// Defines the port the app will run on. Defaults to 8080, but can be overridden
+// when starting the server. Example command to overwrite PORT env variable value:
+// PORT=9000 npm start
+const port = process.env.PORT || 8080;
 const app = express();
 
-// Define middlewares to enable cors and json body parsing
+// Add middlewares to enable cors and json body parsing
 app.use(cors());
 app.use(express.json());
 
-// Define a simple route
+// Start defining your routes here
 app.get("/", (req, res) => {
   res.send("Hello Technigo!");
 });
-
-// Define the port the app will run on
-const port = process.env.PORT || 8080;
 
 // Start the server
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
 
-// Create a user with hashed password and save to MongoDB
-const saltRounds = 10; // You can adjust the number of salt rounds based on your requirements
-const hashedPassword = bcrypt.hashSync("foobar", saltRounds);
-const user = new User({ name: "Bob", password: hashedPassword });
-user.save();
-
-// Log the hashed password to the console
-console.log(bcrypt.hashSync("foobar"));
+console.log(bcrypt.hashSync("foobar"))
