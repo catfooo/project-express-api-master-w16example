35fb9b58c773d50c47b759fa37983da374b79a2b
So Youn Choi <manghatuhyo@gmail.com>
2023-12-02 11:41:48 +0100

    missing salt argument fixed by chatgpt. but why this works and where is encrypted version of foobar?

--- server.js
+++ server.js
@@ -5,23 +5,28 @@ import mongoose from 'mongoose'
 import bcrypt from 'bcrypt'
 
 const User = mongoose.model('User', {
-  name:{
+  name: {
     type: String,
     unique: true
   },
-  password:{
+  password: {
     type: String,
     required: true
   },
-  accessToken:{
+  accessToken: {
     type: String,
     default: () => crypto.randomBytes(128).toString('hex')
   }
 })
 
 // One-way encryption
-const user = new User({name: "Bob", password: bcrypt.hashSync("foobar")})
-user.save()
+const saltRounds = 10; // You can adjust the number of salt rounds based on your security requirements
+const password = "foobar";
+const salt = bcrypt.genSaltSync(saltRounds);
+const hashedPassword = bcrypt.hashSync(password, salt);
+
+const user = new User({ name: "Bob", password: hashedPassword });
+user.save();
 
 const port = process.env.PORT || 8080;
 const app = express();
@@ -35,7 +40,6 @@ app.get("/", (req, res) => {
 
 // Endpoint to log hashed password
 app.get("/hashed-password", (req, res) => {
-  const hashedPassword = bcrypt.hashSync("foobar");
   console.log("Hashed Password:", hashedPassword);
   res.send("Check the console for the hashed password");
 });
